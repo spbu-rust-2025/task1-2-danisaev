68f35fe56b8f113f067183731c07d99fbfb39b66
Daniil Isaev <isaev.daniil06@gmail.com>
2025-10-25 16:09:06 +0300

    Add a file of second task from the first homework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut sum_of_numbers = 0;
+    loop {
+        let mut input_number = String::new();
+        io::stdin().read_line(&mut input_number).unwrap();
+
+        let trimmed_number = input_number.trim();
+        if trimmed_number == "-1" {
+            break;
+        }
+
+        match trimmed_number.parse::<i32>() {
+            Ok(num) => {
+                if num > 0 {
+                    sum_of_numbers += num;
+                } else {
+                    println!("NaN");
+                    return;
+                }
+            }
+            Err(_) => {
+                println!("NaN");
+                return;
+            }
+        }
+    }
+    println!("{}", sum_of_numbers);
 }
